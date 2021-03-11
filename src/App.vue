c7b90cfe86f1c220ad9aa8be8deced4715888e5e
Nazar Filippov <nnnazayura@gmail.com>
2021-03-11 08:42:44 +0200

    Add history save

--- src/App.vue
+++ src/App.vue
@@ -59,9 +59,31 @@
 
       <section v-if="allTickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <div v-show="btnOn">
+          <button
+            @click="page = page - 1"
+            v-if="page !== 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Назад
+          </button>
+          {{ page }}
+          <button
+            @click="page = page + 1"
+            v-if="6 * (page + 1) - allTickers.length < 6"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Вперёд
+          </button>
+        </div>
+        <div>
+          <p>Фильтр: <input v-model="filter" /></p>
+        </div>
+
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="ticker in allTickers"
+            v-for="ticker in filteredTickers()"
             :key="ticker.name"
             @click="selectTicker(ticker)"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
@@ -157,7 +179,10 @@ export default {
       graph: [],
       chekAdd: false,
       hintsData: [],
-      hintsReady: []
+      hintsReady: [],
+      page: 1,
+      filter: "",
+      btnOn: true
     };
   },
   methods: {
@@ -168,7 +193,10 @@ export default {
         return false;
       }
 
+      this.chekAdd = false;
       this.hintsReady = [];
+
+      this.filter = "";
 
       val = val.trim().split();
       val.forEach(function(item, i, arr) {
@@ -233,6 +261,7 @@ export default {
     },
     addedBefore(ticker) {
       ticker = ticker
+        .trim()
         .split("")
         .map(x => x.toUpperCase())
         .join("");
@@ -246,9 +275,12 @@ export default {
     },
     hintsSearch(ticker) {
       ticker = ticker
+        .trim()
         .split("")
         .map(x => x.toUpperCase())
         .join("");
+      if (ticker == "") return false;
+
       let allCoins = Object.keys(this.hintsData);
       console.log(allCoins);
       let allHints = [];
@@ -260,13 +292,44 @@ export default {
       });
       console.log(allHints.slice(0, 4));
       return (this.hintsReady = allHints.slice(0, 4));
+    },
+    // filteredTickers() {
+    //   return this.allTickers.filter(ticker => ticker.includes(this.filter));
+    // },
+
+    filteredTickers() {
+      if (this.filter.trim() == "") {
+        this.btnOn = true;
+        let start = (this.page - 1) * 6;
+        let end = this.page * 6;
+        return this.allTickers.slice(start, end);
+      } else {
+        this.btnOn = false;
+        return this.allTickers.filter(ticker =>
+          ticker.name.toLowerCase().includes(this.filter.toLowerCase())
+        );
+      }
     }
   },
-  computed: {},
   watch: {
     ticker: function() {
-      console.log(this.ticker);
-      /* eslint-disable */
+      if (this.ticker.trim() == "") this.hintsReady = [];
+    },
+    filter: function() {
+      //пушим в историю браузера текущий фильтр и страницу
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page: function() {
+      console.log(this.page);
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
     }
   },
   created() {
@@ -278,6 +341,17 @@ export default {
       console.log(data.Data);
       this.hintsData = data.Data;
     }, 100);
+
+    let windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+    //берем из истории виндова параметры filter и page, и если они были - приписываем текущим параметрам
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
 
     const tickersData = localStorage.getItem("cryptonomicon-tickers-list");
     // console.log(tickersData);
